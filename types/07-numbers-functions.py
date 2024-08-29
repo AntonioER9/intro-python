b5be1e2d4f490966dcb410e3af7d07c4911225b4
AntonioER9 <aespinozareyes9@gmail.com>
2024-08-29 18:33:24 -0400

    add math import

---
+++ types/07-numbers-functions.py
@@ -0,0 +1,14 @@
+import math
+
+print(round(1.3))  # Round to the nearest integer
+print(abs(-1.3))  # Absolute value
+
+print(math.ceil(1.3))  # Round up
+print(math.floor(1.3))  # Round down
+print(math.isnan(1.3))  # Is Not a Number?
+print(math.pow(2, 3))  # 2^3
+print(math.sqrt(4))  # Square root
+print(math.pi)  # Pi
+print(math.e)  # Euler's number
+print(math.inf)  # Infinity
+print(-math.inf)  # Negative infinity
