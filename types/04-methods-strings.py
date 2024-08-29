caa0e6d1ec764ff4ad2cd09c7ccc594e9fe2852c
AntonioER9 <aespinozareyes9@gmail.com>
2024-08-29 18:22:51 -0400

    add methods

---
+++ types/04-methods-strings.py
@@ -0,0 +1,12 @@
+animal = " Pig "
+print(animal.upper())  # PIG
+print(animal.lower())  # pig
+print(animal.strip().capitalize())  # Pig
+print(animal.title())  # Pig
+print(animal.strip())  # pig
+print(animal.lstrip())  # Pig
+print(animal.rstrip())  # Pig
+print(animal.find("i"))  # 2
+print(animal.replace("Pig", "Dog"))  # Dog
+print("i" in animal)  # True
+print("i" not in animal)  # False
