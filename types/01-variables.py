1f26d3790bded4cc689698fc14e947a368e4b9cc
AntonioER9 <aespinozareyes9@gmail.com>
2024-08-29 17:59:05 -0400

    add types

---
+++ types/01-variables.py
@@ -0,0 +1,4 @@
+name_course = "Ultimate Python Course"
+print(name_course)
+students = 100
+published = True
---
+++ types/03-format-strings.py
@@ -0,0 +1,4 @@
+name = "Antonio"
+surname = "Espinoza"
+complete_name = f"{name} {surname}"
+print(complete_name)  # Antonio Espinoza
