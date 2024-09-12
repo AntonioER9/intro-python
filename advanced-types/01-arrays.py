a204403ffa3fef0b43263e6c1b0a53982d895b73
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-12 11:49:28 -0300

    Add arrays with different data types and sizes

---
+++ advanced-types/01-arrays.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5]
+strings = ["a", "b", "c", "d", "e"]
+words = ["apple", "banana", "cherry", "date", "elderberry"]
+mixed = [1, "two", 3, "four", 5]
+booleans = [True, False, True, False, True]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]  # 3x3 matrix
+zeros = [0] * 5  # [0, 0, 0, 0, 0]
+range = list(range(5))  # [0, 1, 2, 3, 4]
